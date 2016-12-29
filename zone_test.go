9e31509d3e73d2e2045d514f4d22d71192699191
delphinus <delphinus@remora.cx>
2016-12-30 01:45:17 +0900

    Add simple test for error in bad JSON

--- zone_test.go
+++ zone_test.go
@@ -3,6 +3,7 @@ package dozens
 import (
 	"fmt"
 	"net/http"
+	"strings"
 	"testing"
 
 	"github.com/jarcoal/httpmock"
@@ -26,6 +27,26 @@ func TestDoZoneRequestStatusNotOK(t *testing.T) {
 	result := errors.Cause(err).Error()
 
 	if result != expected {
+		t.Errorf("expected '%s', bug got '%s'", expected, result)
+	}
+}
+
+func TestDoZoneRequestBadJSON(t *testing.T) {
+	httpmock.Activate()
+	defer httpmock.DeactivateAndReset()
+
+	method := "GET"
+	hogeURL := "http://hoge.com"
+	badJSON := "{hoge}"
+
+	httpmock.RegisterResponder(method, hogeURL, httpmock.NewStringResponder(http.StatusOK, badJSON))
+	req, _ := http.NewRequest(method, hogeURL, nil)
+	expected := "error in Decode"
+
+	_, err := doZoneRequest(req)
+	result := err.Error()
+
+	if strings.Index(result, expected) != 0 {
 		t.Errorf("expected '%s', bug got '%s'", expected, result)
 	}
 }
