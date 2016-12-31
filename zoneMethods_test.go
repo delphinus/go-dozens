384978a300c36846db187a8e61d7042821abdc61
delphinus <delphinus@remora.cx>
2016-12-31 14:18:26 +0900

    Simplify logic for tests

--- zoneMethods_test.go
+++ zoneMethods_test.go
@@ -10,27 +10,21 @@ import (
 	"github.com/jarcoal/httpmock"
 )
 
-type zoneMockOptions struct {
-	Method    string
-	URL       string
-	Status    int
-	DoRequest func() (ZoneResponse, error)
+type dozensMock struct {
+	Method   string
+	URL      string
+	Status   int
+	Response interface{}
 }
 
-var validZoneResponse = ZoneResponse{
-	Domain: []domain{
-		domain{ID: "hoge", Name: "fuga"},
-	},
-}
-
-func zoneMock(options zoneMockOptions) (ZoneResponse, error) {
+func (m dozensMock) Do(reqFunc func() (interface{}, error)) (interface{}, error) {
 	httpmock.Activate()
 	defer httpmock.DeactivateAndReset()
 
-	responder, _ := httpmock.NewJsonResponder(options.Status, validZoneResponse)
-	httpmock.RegisterResponder(options.Method, options.URL, responder)
+	responder, _ := httpmock.NewJsonResponder(m.Status, m.Response)
+	httpmock.RegisterResponder(m.Method, m.URL, responder)
 
-	return options.DoRequest()
+	return reqFunc()
 }
 
 func TestZoneCreateWithError(t *testing.T) {
@@ -48,17 +42,21 @@ func TestZoneCreateWithError(t *testing.T) {
 }
 
 func TestZoneCreateValidResponse(t *testing.T) {
-	body := ZoneCreateBody{
-		Name:            "hoge",
-		AddGoogleApps:   true,
-		GoogleAuthorize: "hoge",
-		MailAddress:     "hoge",
+	validZoneResponse := ZoneResponse{
+		Domain: []domain{
+			domain{ID: "hoge", Name: "fuga"},
+		},
 	}
-	_, err := zoneMock(zoneMockOptions{
-		Method:    methodPost,
-		URL:       endpoint.ZoneCreate().String(),
-		Status:    http.StatusOK,
-		DoRequest: func() (ZoneResponse, error) { return ZoneCreate("", body) },
+
+	mock := dozensMock{
+		Method:   methodPost,
+		URL:      endpoint.ZoneCreate().String(),
+		Status:   http.StatusOK,
+		Response: validZoneResponse,
+	}
+
+	_, err := mock.Do(func() (interface{}, error) {
+		return ZoneCreate("", ZoneCreateBody{})
 	})
 
 	if err != nil {
