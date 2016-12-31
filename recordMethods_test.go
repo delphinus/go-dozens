eaab1ce0c6034df4e37f2c7455ded8b2b78cecf4
delphinus <delphinus@remora.cx>
2016-12-31 16:31:14 +0900

    Add tests for recordMethods

--- recordMethods_test.go
+++ recordMethods_test.go
@@ -50,3 +50,93 @@ func TestRecordListValidResponse(t *testing.T) {
 		t.Errorf("error: %s", err)
 	}
 }
+
+func TestRecordCreateWithError(t *testing.T) {
+	originalMethodPost := methodPost
+	methodPost = "(" // invalid method
+	defer func() { methodPost = originalMethodPost }()
+
+	_, err := RecordCreate("", RecordCreateBody{})
+
+	expected := "error in MakePost"
+	result := err.Error()
+	if strings.Index(result, expected) != 0 {
+		t.Errorf("error does not found: %s", result)
+	}
+}
+
+func TestRecordCreateValidResponse(t *testing.T) {
+	mock := dozensMock{
+		Method:   methodPost,
+		URL:      endpoint.RecordCreate().String(),
+		Response: validRecordResponse,
+	}
+
+	_, err := mock.Do(func() (interface{}, error) {
+		return RecordCreate("", RecordCreateBody{})
+	})
+
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+}
+
+func TestRecordUpdateWithError(t *testing.T) {
+	originalMethodPost := methodPost
+	methodPost = "(" // invalid method
+	defer func() { methodPost = originalMethodPost }()
+
+	_, err := RecordUpdate("", "", RecordUpdateBody{})
+
+	expected := "error in MakePost"
+	result := err.Error()
+	if strings.Index(result, expected) != 0 {
+		t.Errorf("error does not found: %s", result)
+	}
+}
+
+func TestRecordUpdateValidResponse(t *testing.T) {
+	mock := dozensMock{
+		Method:   methodPost,
+		URL:      endpoint.RecordUpdate("").String(),
+		Response: validRecordResponse,
+	}
+
+	_, err := mock.Do(func() (interface{}, error) {
+		return RecordUpdate("", "", RecordUpdateBody{})
+	})
+
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+}
+
+func TestRecordDeleteWithError(t *testing.T) {
+	originalMethodDelete := methodDelete
+	methodDelete = "(" // invalid method
+	defer func() { methodDelete = originalMethodDelete }()
+
+	_, err := RecordDelete("", "")
+
+	expected := "error in MakeDelete"
+	result := err.Error()
+	if strings.Index(result, expected) != 0 {
+		t.Errorf("error does not found: %s", result)
+	}
+}
+
+func TestRecordDeleteValidResponse(t *testing.T) {
+	mock := dozensMock{
+		Method:   methodDelete,
+		URL:      endpoint.RecordDelete("").String(),
+		Response: validRecordResponse,
+	}
+
+	_, err := mock.Do(func() (interface{}, error) {
+		return RecordDelete("", "")
+	})
+
+	if err != nil {
+		t.Errorf("error: %v", err)
+	}
+}
